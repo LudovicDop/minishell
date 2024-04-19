b0b500a5a910081faf90b9bbf815eeb060b963e5
Ludovic Doppler <ldoppler@k2r3p3.42mulhouse.fr>
2024-04-19 15:30:00 +0200

    test

--- minishell_main.c
+++ minishell_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_main.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ludovicdoppler <ludovicdoppler@student.    +#+  +:+       +#+        */
+/*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 18:10:56 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/04/19 11:01:56 by ludovicdopp      ###   ########.fr       */
+/*   Updated: 2024/04/19 15:29:44 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
