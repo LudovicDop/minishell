8683694cdc74abdc5ec4601b2d3c9f285d46243b
Ludovic Doppler <ldoppler@k1r2p15.42mulhouse.fr>
2024-06-03 23:17:38 +0200

    Have to be careful still have some seg fault with the cmd cd idk where exactly

--- srcs/exec/builtins/ft_cd/ft_cd.c
+++ srcs/exec/builtins/ft_cd/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 01:36:43 by ludovicdopp       #+#    #+#             */
-/*   Updated: 2024/06/03 23:05:08 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/06/03 23:15:32 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -205,15 +205,15 @@ void    ft_cd(t_envp **envp, char *path)
     }
     if (path[0] == '/')
     {
-        new_node = malloc(sizeof(t_pwd));
-        new_node->node = ft_strdup(getenv("HOME"));
-        printf("null path : %s\n", new_node->node);
-        new_node->next = NULL;
         if (chdir(path) < 0)
         {
             perror("chdir");
             return ;
         } 
+        new_node = malloc(sizeof(t_pwd));
+        new_node->node = ft_strdup(path);
+        printf("null path : %s\n", new_node->node);
+        new_node->next = NULL;
         ft_add_pwd_node(&pwd_lst, new_node);
         init_pwd_w_envp(envp, &pwd_lst);
         return ;
