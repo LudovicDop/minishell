5141629ff1348967ef5a85be5a0de91827c1b787
Ludovic Doppler <ldoppler@k2r3p2.42mulhouse.fr>
2024-07-05 14:23:46 +0200

    heredoc in progress

--- srcs/exec/redirection/ft_heredoc.c
+++ srcs/exec/redirection/ft_heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_heredoc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ludovicdoppler <ludovicdoppler@student.    +#+  +:+       +#+        */
+/*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:08:25 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/07/05 13:24:48 by ludovicdopp      ###   ########.fr       */
+/*   Updated: 2024/07/05 14:23:18 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,8 @@ int	ft_heredoc(t_token *node, int *pipe_fd, t_token *token)
 {
 	char	*tmp;
 	char	*full_string;
-	int		length;
 	int		max;
 
-	length = 0;
 	tmp = NULL;
 	full_string = NULL;
 	if (!node && node->type != HEREDOC)
@@ -45,7 +43,6 @@ int	ft_heredoc(t_token *node, int *pipe_fd, t_token *token)
 		write(1, "> ", 2);
 		tmp = get_next_line(STDIN_FILENO);
 		remove_return_line(tmp);
-		printf("\033[36;1m%s && %s\033[m\n", node->value, tmp);
 		if (tmp && ft_strcmp(node->value, tmp) != 0)
 		{
 			if (tmp)
@@ -55,17 +52,18 @@ int	ft_heredoc(t_token *node, int *pipe_fd, t_token *token)
 			}
 			full_string = ft_strjoin2(full_string, tmp);
 		}
-		printf("\033[31;1mfull_string : %s\033[m\n", full_string);
 		if (tmp == NULL || tmp[0] == '\0')
 			return (free(full_string), free(tmp), 1);
 		else if (ft_strcmp(node->value, tmp) == 0)
 		{
-			pipe(pipe_fd);
-			length = ft_strlen(full_string);
-			write(pipe_fd[WRITE], full_string, length);
-			dup2(pipe_fd[READ], STDIN_FILENO);
-			close(pipe_fd[READ]);
-			close(pipe_fd[WRITE]);
+			if (node->next->type != HEREDOC)
+			{
+				pipe(pipe_fd);
+				write(pipe_fd[WRITE], full_string, ft_strlen(full_string));
+				dup2(pipe_fd[READ], STDIN_FILENO);
+				close(pipe_fd[READ]);
+				close(pipe_fd[WRITE]);
+			}
 			free(tmp);
 			free(full_string);
 			break ;
