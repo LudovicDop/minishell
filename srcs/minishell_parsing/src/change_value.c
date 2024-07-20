d227b3f9f2d5a3906ca8fc36d0c58524f64c7815
Alain Phan <alphan@k2r2p9.42mulhouse.fr>
2024-07-20 17:18:39 +0200

    modif echo

--- srcs/minishell_parsing/src/change_value.c
+++ srcs/minishell_parsing/src/change_value.c
@@ -114,6 +114,7 @@ void	change_for_value2(t_token *token, t_envp **envp, char *str)
 	t_token	*current;
 	int		i;
 	t_envp	*tmp;
+	char	*str2;
 
 	current = token;
 	while (current)
@@ -130,24 +131,21 @@ void	change_for_value2(t_token *token, t_envp **envp, char *str)
 		}
 		else if (current->value[0] == '$')
 		{
-			// printf("current->value = %s\n", current->value);
 			tmp = search_envp_key(envp, current->value + 1);
-			// printf("tmp->value = %s\n", tmp->value);
 			free(current->value);
 			if (tmp->value)
 				current->value = ft_strdup(tmp->value);
-			// current->value = ft_strdup(tmp->value);
 		}
 		else if (current->value[0] == '~')
 		{
 			tmp = search_envp_key(envp, "HOME");
 			free(current->value);
 			current->value = ft_strdup(str);
-			// free(str);
 		}
 		current = current->next;
 	}
 }
+
 void	change_for_value(t_token *token, t_envp **envp, char *str)
 {
 	t_token	*current;
@@ -155,80 +153,31 @@ void	change_for_value(t_token *token, t_envp **envp, char *str)
 	t_envp	*tmp;
 
 	current = token;
-	while (current)
+	if (!ft_strncmp(current->value, "$?", 3))
 	{
-		if (!ft_strncmp(current->value, "$?", 3))
-		{
-			free(current->value);
-			current->value = ft_itoa(g_signal);
-		}
-		else if (!ft_strncmp(current->value, "$$", 3))
-		{
-			free(current->value);
-			current->value = ft_itoa(getpid());
-		}
-		else if (current->value[0] == '$')
-		{
-			// printf("current->value = %s\n", current->value);
-			tmp = search_envp_key(envp, current->value + 1);
-			// printf("tmp->value = %s\n", tmp->value);
-			free(current->value);
-			if (tmp->value)
-				current->value = ft_strdup(tmp->value);
-			// current->value = ft_strdup(tmp->value);
-		}
-		else if (current->value[0] == '~')
-		{
-			tmp = search_envp_key(envp, "HOME");
-			free(current->value);
-			current->value = ft_strdup(str);
-			// free(str);
-		}
-		current = current->next;
+		free(current->value);
+		current->value = ft_itoa(g_signal);
+	}
+	else if (!ft_strncmp(current->value, "$$", 3))
+	{
+		free(current->value);
+		current->value = ft_itoa(getpid());
+	}
+	else if (current->value[0] == '$')
+	{
+		// printf("current->value = %s\n", current->value);
+		tmp = search_envp_key(envp, current->value + 1);
+		// printf("tmp->value = %s\n", tmp->value);
+		free(current->value);
+		if (tmp->value)
+			current->value = ft_strdup(tmp->value);
+		// current->value = ft_strdup(tmp->value);
+	}
+	else if (current->value[0] == '~')
+	{
+		tmp = search_envp_key(envp, "HOME");
+		free(current->value);
+		current->value = ft_strdup(str);
+		// free(str);
 	}
 }
-
-/*
-{
-	t_token	*current;
-	char	*str;
-	char	tmp[] = "le chemion du path";
-	char	*str2;
-	int		i;
-	int		j;
-
-	current = token;
-	i = 0;
-	// remplace $? par le code de retour exit_status
-	j = 0;
-	while (current)
-	{
-		if (current->type == CMD && !ft_strncmp(current->value, "$", 1))
-		{
-			if (!ft_strncmp(current->value, "$?", 2))
-			{
-				free(current->value);
-				current->value = ft_itoa(i);
-			}
-			else if (!ft_strncmp(current->value, "$$", 2))
-			{
-				free(current->value);
-				current->value = ft_itoa(i);
-			}
-			else if (!ft_strncmp(current->value, "$PATH", 6))
-			{
-				free(current->value);
-				current->value = ft_strdup("le chemion du path");
-			}
-		}
-		else if (current->type == CMD && !ft_strncmp(current->value, "~", 1))
-		{
-			// str = ft_strjoin("/Users/", current->value + 1);
-			free(current->value);
-			// current->value = ft_strdup(str);
-			free(str);
-		}
-		current = current->next;
-	}
-}
-*/
