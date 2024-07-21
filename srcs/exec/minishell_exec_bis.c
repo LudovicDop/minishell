6191a28042560b146077dac3e9ac0feb1ec859df
Ludovic Doppler <ldoppler@k2r4p7.42mulhouse.fr>
2024-07-21 17:25:24 +0200

    maj builtins

--- srcs/exec/minishell_exec_bis.c
+++ srcs/exec/minishell_exec_bis.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 14:27:41 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/07/21 16:46:48 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/07/21 17:25:01 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ int	ft_single_cmd(t_lexer *node, t_glob *glob, int *pipe_fd, t_envp **envp_list)
 	if (node->type == CMD && how_many_cmd(glob->root) == 1
 		&& ft_command_after(node) && !ft_is_it_pipe(glob->root))
 	{
-		if (!search_builtins_token(glob->root, envp_list, glob))
+		if (!search_builtins_token(glob->root, envp_list, glob, pipe_fd))
 			execute_command(node, pipe_fd, *envp_list, glob);
 		dup2(glob->fd_in_old, STDIN_FILENO);
 		if (glob->fd_in_old == -1)
