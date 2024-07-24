4acc62d54f2e5be11220bd92bfe7c9f2ab1ac372
LudovicDop <ludovicdop@gmail.com>
2024-07-24 09:56:25 +0200

    fix open fd issue

--- srcs/exec/redirection/ft_heredoc/ft_heredoc.c
+++ srcs/exec/redirection/ft_heredoc/ft_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:08:25 by ldoppler          #+#    #+#             */
-/*   Updated: 2024/07/23 08:53:10 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/07/24 09:53:49 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,8 +111,12 @@ int	ft_heredoc(t_lexer *node, int *pipe_fd, t_glob *glob, t_envp *envp_list)
 		return (0);
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, SIG_IGN);
-	if (pipe(pipe_fd) < 0)
-		return (1);
+	if (!ft_is_it_pipe(glob->root))
+	{
+		fprintf(stderr, "\033[31;1mStart pipe\033[m\n");
+		if (pipe(pipe_fd) < 0)
+			return (1);
+	}
 	id = fork();
 	if (id < 0)
 		return (close(pipe_fd[WRITE]), close(pipe_fd[READ]), 1);
